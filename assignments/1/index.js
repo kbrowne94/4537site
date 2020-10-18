4784869f0e2da13c62ffd97db36c43a51c76fd18
kylebr <kbrowne.3694@gmail.com>
2020-10-17 19:14:14 -0700

    initial memory game

--- assignments/1/index.js
+++ assignments/1/index.js
@@ -1,69 +1,114 @@
 let score = 0;
-let deck = [];
+let num_false_cards = 3;
+let num_positive_cards = 9 -num_false_cards;
+let dim = 3;
+let gamestate = true;
 
 
-$("document").ready(function() {
-    $(".card").trigger('click');
-    setTimeout(function() {
-        $(".card").trigger('click');
-        score = 0
-        $("#score").text(score);
-        $("#score").css("visibility", "visible");;
-
-    },4000)
-});
+// function to grab highscores for leaderboard
 
 class Card {
-    constructor(color){
-        this.color = color;
+    constructor() {
+        this.isClicked = false;
+        this.isFalseCard = false;
     }
 }
-for( let i = 0; i < 49; i++) {
-    $('.memory-game').append(`<div class="card"></div>`);
-    let x = Math.floor(Math.random()*10);
-    if (x%2==0) {
-        deck.push(new Card("red"))
-    } else {
-        deck.push(new Card("blue"))
+
+
+function draw_board(dim) {
+    let b = new Array(dim);
+    for (let i =0; i< b.length; i++){
+        b[i] = new Array(dim);
     }
-    
-};
-
-
-let showCard1 = function() {
-    this.classList.toggle("open-hit");
-    // this.classList.toggle("disabled");
-};
-
-let showCard2 = function() {
-    this.classList.toggle("open-miss");
-    // this.classList.toggle("disabled");
-};
-
-
-let card = document.getElementsByClassName("card");
-let cards = [...card];
-for (let i = 0; i < cards.length; i++){
-    if (deck[i].color == "blue"){
-        cards[i].addEventListener("click", showCard1);
-    }else {
-        cards[i].addEventListener("click", showCard2);
+    for(let i = 0; i < dim; i++) {
+        for(let j = 0; j< dim; j++) {
+            b[i][j] = new Card();
+        }
     }
+    return b;
+}
 
- };
-
-
-
- 
-$(".card").on("click", function(e){
-    const cardClasses = e.currentTarget.classList;
-    if(cardClasses[1] == 'open-hit') {
-        score++;
-        
+function setFalseCards(board, falseCardCount) {
+    let falseCards = 0;
+    while (falseCards < falseCardCount){
+        let x = Math.floor(Math.random()*board.length);
+        let y = Math.floor(Math.random()*board[0].length);
+        if(!board[x][y].isFalseCard){
+            board[x][y].isFalseCard = true;
+            falseCards+=1;
+        }
     }
-    else {
-        score--;
+}
+// let board = draw_board(3);
+// setFalseCards(board, num_false_cards);
+
+document.getElementById("start").onclick = function () {
+    if(score > 0 && num_false_cards < dim*dim-5){
+        num_false_cards+=4;
+        if (dim < 7){
+            dim++;
+        }
     }
-    $("#score").text(score)
-});
+    this.classList.add('hide');
+    num_positive_cards = dim*dim - num_false_cards;
+    board = draw_board(dim);
+    setFalseCards(board, num_false_cards);
+    create_table(board);
+}
+
+function create_table(board) {
+    let total_rows = dim;
+    let total_cols = dim;
+    let gameBoard = document.getElementById("board");
+    gameBoard.querySelectorAll('*').forEach(n => n.remove());
+    // gameBoard = newGame;
+    let table1 = document.createElement("table");
+    table1.id = "MemoryGame";
+    table1.classList.add('center');
+    for(let r = 0; r < total_rows; r++){
+        let row = document.createElement("tr");
+        row.classList.add('boardrow');
+        for(let c = 0; c < total_cols; c++){
+            let cell = document.createElement("td");
+            cell.classList.add("card")
+            if(board[r][c].isFalseCard){
+                cell.classList.toggle('open-miss');
+                setTimeout(function(){
+                    cell.classList.toggle('open-miss');
+                },3000)
+            } else {
+                cell.classList.toggle('open-hit');
+                setTimeout(function(){
+                    cell.classList.toggle('open-hit');
+                },3000)
+            }
+
+            cell.onclick = function () {
+                if(!board[r][c].isClicked) {
+                    board[r][c].isClicked = true
+
+                    if(board[r][c].isFalseCard) {
+                        cell.classList.toggle('open-miss');
+                        score--;
+                        document.getElementById('score').innerHTML = score;
+                    }else {
+                        cell.classList.toggle('open-hit');
+                        num_positive_cards--;
+                        score++;
+                        document.getElementById('score').innerHTML = score;
+                        if(num_positive_cards === 0){
+                            setTimeout(function(){
+                                document.getElementById("start").click();
+                            }, 500);
+                            
+                        }
+                    }
+                }
+            }
+            row.appendChild(cell);
+        }
+        table1.appendChild(row)
+    }
+    gameBoard.appendChild(table1);
+}
 
